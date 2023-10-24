a6b1e8f024acfa1b3af7015d5887a0ebe616d5d6
Szymon Czudowski <szymon.czudowski@gmail.com>
2023-10-24 02:51:33 +0200

    FEAT: all functions for calculator and exception if divided by 0

--- scripts.js
+++ scripts.js
@@ -18,3 +18,22 @@ function clearInput() {
     display.value = "";
 }
 
+function calculateResult() {
+    const parts = input.split(operator);
+    const num1 = parseFloat(parts[0]);
+    const num2 = parseFloat(parts[1]);
+
+    if (operator === "+") {
+        display.value = num1 + num2;
+    } else if ( operator === "-") {
+        display.value = num1 - num2;
+    } else if ( operator === "*") {
+        display.value = num1 * num2;
+    } else if ( operator === "/") {
+        if ( num2  === 0 ) {
+            display.value = "Can not divide by 0";
+        } else {
+            display.value = num1 / num2;
+        }
+    }
+}
